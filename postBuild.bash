035d9580d5be78d10af4bc157bcdfeb949cae291
Daniel Ritchie <daniel@brainwavecollective.ai>
2024-09-30 14:57:10 -0600

    troubleshooting

--- postBuild.bash
+++ postBuild.bash
@@ -72,10 +72,9 @@ EOM
 
 # brain wave collective for twelvelabs jockey 
 sudo apt-get install -y python3.11
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
-
-sudo apt-get install -y python3.11-venv python3.11-distutils
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
+#sudo apt-get install -y python3.11-venv python3.11-distutils
 
 sudo apt-get install -y ffmpeg
 
