613a018151775ff277a3f7c7fd2c3be8a406d18a
Daniel Ritchie <daniel@brainwavecollective.ai>
2024-09-30 14:36:58 -0600

    say yes

--- postBuild.bash
+++ postBuild.bash
@@ -71,13 +71,13 @@ EOM
 
 
 # brain wave collective for twelvelabs jockey 
-sudo apt-get install python3.11
+sudo apt-get install -y python3.11
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
 
-sudo apt-get install python3.11-venv python3.11-distutils
+sudo apt-get install -y python3.11-venv python3.11-distutils
 
-sudo apt-get install ffmpeg
+sudo apt-get install -y ffmpeg
 
 
 #### NOT TESTED
