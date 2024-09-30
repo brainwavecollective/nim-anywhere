40e410a7cb06b742bae57a7a687349dc06320f35
Daniel Ritchie <daniel@brainwavecollective.ai>
2024-09-29 19:07:37 -0600

    added some prereqs for jockey'

--- postBuild.bash
+++ postBuild.bash
@@ -69,6 +69,33 @@ if [ -f /project/code/config_sample.yaml ] && [ ! -f /project/code/config.yaml ]
 fi
 EOM
 
+
+# brain wave collective for twelvelabs jockey 
+sudo apt-get install python3.11
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
+
+sudo apt-get install python3.11-venv python3.11-distutils
+
+sudo apt-get install ffmpeg
+
+
+#### NOT TESTED
+
+#### Setup python virtual env 
+
+#cd tl-jockey && python3 -m venv venv
+#source venv/bin/activate
+#echo "the virtual environment is: $VIRTUAL_ENV"
+#pip3 install -r requirements.txt
+
+
+#### RUN 
+#source venv/bin/activate
+#python3 -m jockey terminal
+
+
+
 # clean up
 sudo apt-get autoremove -y
 sudo rm -rf /var/cache/apt
